bb89db0b3b0fcdf6723886e0e80b97618f7c36fe
dhopp1 <daniel.hopp1@gmail.com>
2020-11-03 10:31:57 +0100

    Adding ability to do two quarters at once

--- gen_news_graphs.r
+++ gen_news_graphs.r
@@ -12,25 +12,26 @@ unctad_nowcast_web_directory <- "/home/danhopp/dhopp1/UNCTAD/unctad-nowcast-web/
 catalog <- read_csv(paste0(helper_directory, "catalog.csv"))
 
 # parameter, which quarter to nowcast
-target_period <- as.Date("2020-09-01")
-
-# generating new plots and forecasts
-database_dates <- get_databases(output_directory)$database_dates
-latest_database <- get_databases(output_directory)$latest_database
-data <- read_csv(paste0(output_directory, latest_database, "_database_tf.csv")) %>% data.frame
-
-for (target_variable in c("x_world", "x_servs_world", "x_vol_world2")) {
-  reestimate <- list.files(paste0("estimated_models/", target_variable, "/")) %>% # last estimated model
-    sort %>% 
-    .[length(.)] %>% 
-    paste0("estimated_models/", target_variable, "/", .)
-  save_directory <- "output/"
-  gen_plots(database_dates, latest_database, target_variable, target_period, reestimate, save_directory) 
-}
-
-# updating data file of unctad nowcast website
-if (exists("unctad_nowcast_web_directory")) {
-  update_nowcast_web_data(unctad_nowcast_web_directory, latest_database, target_period, output_directory)
+for (target_period in c("2020-09-01", "2020-12-01")) {
+  target_period <- as.Date(target_period)
+  # generating new plots and forecasts
+  database_dates <- get_databases(output_directory)$database_dates
+  latest_database <- get_databases(output_directory)$latest_database
+  data <- read_csv(paste0(output_directory, latest_database, "_database_tf.csv")) %>% data.frame
+  
+  for (target_variable in c("x_world", "x_servs_world", "x_vol_world2")) {
+    reestimate <- list.files(paste0("estimated_models/", target_variable, "/")) %>% # last estimated model
+      sort %>% 
+      .[length(.)] %>% 
+      paste0("estimated_models/", target_variable, "/", .)
+    save_directory <- "output/"
+    gen_plots(database_dates, latest_database, target_variable, target_period, reestimate, save_directory) 
+  }
+  
+  # updating data file of unctad nowcast website
+  if (exists("unctad_nowcast_web_directory")) {
+    update_nowcast_web_data(unctad_nowcast_web_directory, latest_database, target_period, output_directory)
+  } 
 }
 
 cat("\033[0;32mNowcasts successfully updated\033[0m\n")
